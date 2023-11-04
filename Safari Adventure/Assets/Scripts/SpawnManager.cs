b82da6c8b4f01db948cf164447fe7dc13602cd03
DavidThorn03 <b00152842@mytudublin.ie>
2023-11-04 20:52:06 +0000

    Spawn Manager

--- Safari Adventure/Assets/Scripts/SpawnManager.cs
+++ Safari Adventure/Assets/Scripts/SpawnManager.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalPrefabs;
-    private float spawnRangeX = 15;
-    private float spawnRangeZ = 15;
-    private float startDelay = 2;
-    private float startInterval = 1.5f;
-
+    public GameObject enemy;
+    private float spawnRangex = 10;
+    private float spawnPosz = 20;
+    private float startDelay = 1.5f;
+    private float spawnInterval = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, startInterval);
+        InvokeRepeating("SpawnEnemy", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0 , spawnRangeZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        
+    }
+    void SpawnEnemy()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangex, spawnRangex), 2, spawnPosz);
+        Instantiate(enemy, spawnPos, enemy.transform.rotation);
     }
 }
