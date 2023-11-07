3b7cb5d6fa7cebb3034db15eb28b16498edc0187
alex-24-cyber <B00151494@mytudublin.ie>
2023-11-07 10:49:48 +0000

    PowerUp

---
+++ Safari Adventure/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PowerUp : MonoBehaviour
+{
+    public float doubleJumpForce = 10f; 
+
+    private bool collected = false;
+}
